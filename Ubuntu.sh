12853db2bab534487639a0732088b848798a74eb
Fardantz <ahmadfardan1140@gmail.com>
2024-11-19 11:04:16 +0700

    Update Ubuntu.sh

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -59,19 +59,6 @@ subnet 192.168.3.0 netmask 255.255.255.0 {
   default-lease-time 600;
   max-lease-time 7200;
 }
-
-subnet 192.168.20.0 netmask 255.255.255.0 { 
-  range 192.168.20.10 192.168.20.100; 
-  option routers 192.168.20.1; 
-  option subnet-mask 255.255.255.0; 
-  option domain-name-servers 8.8.8.8, 8.8.4.4;
-#  option domain-name "internal.example.org";
-  option subnet-mask 255.255.255.0;
-  option routers 192.168.20.1;
-  option broadcast-address 192.168.20.255;
-  default-lease-time 600;
-  max-lease-time 7200;
-}
 EOF
 
 sudo tee /etc/default/isc-dhcp-server << EOF

7fbc403f632d32de0e0bce8bafc7587fbaf2f552
Fardantz <ahmadfardan1140@gmail.com>
2024-11-19 10:34:23 +0700

    Revisi

--- Mikrotik.sh
+++ Mikrotik.sh
@@ -5,24 +5,24 @@ add name=vlan20 vlan-id=20 interface=ether1
 
 # Assign IP address ke VLAN
 /ip address
-add address=192.168.A.1/24 interface=vlan10
-add address=192.168.200.1/24 interface=vlan20
+add address=192.168.3.1/24 interface=vlan10
+add address=192.168.20.1/24 interface=vlan20
 
 # Konfigurasi DHCP Server untuk VLAN 10
 /ip pool
-add name=dhcp_pool_vlan10 ranges=192.168.A.10-192.168.A.100
+add name=dhcp_pool_vlan10 ranges=192.168.3.10-192.168.3.100
 /ip dhcp-server
 add name=dhcp_vlan10 interface=vlan10 address-pool=dhcp_pool_vlan10
 /ip dhcp-server network
-add address=192.168.A.0/24 gateway=192.168.A.1 dns-server=8.8.8.8,8.8.4.4
+add address=192.168.3.0/24 gateway=192.168.3.1 dns-server=8.8.8.8,8.8.4.4
 
 # Konfigurasi DHCP Server untuk VLAN 20
 /ip pool
-add name=dhcp_pool_vlan20 ranges=192.168.200.10-192.168.200.100
+add name=dhcp_pool_vlan20 ranges=192.168.20.10-192.168.20.100
 /ip dhcp-server
 add name=dhcp_vlan20 interface=vlan20 address-pool=dhcp_pool_vlan20
 /ip dhcp-server network
-add address=192.168.200.0/24 gateway=192.168.200.1 dns-server=8.8.8.8,8.8.4.4
+add address=192.168.20.0/24 gateway=192.168.20.1 dns-server=8.8.8.8,8.8.4.4
 
 # Aktifkan IP forwarding
 /ip settings set forward=yes
